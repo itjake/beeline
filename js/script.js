30f8a654071d9029aa3b64b94d2b944459f86e1a
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-11-30 11:54:38 +0600

    feat: added fixed logic and style for header

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 (function () {
-    const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header-content');
-    const menuCloseItem = document.querySelector('.header-nav__close');
-    burgerItem.addEventListener('click', () => {
-        menu.classList.add('header__menu__active');
-    });
-    menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header__menu__active');
-    });
-}());
+    const header = document.querySelector(".header");
+    const container = document.querySelector(".container")
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add("header-fixed");
+            container.classList.add("container-fixed");
+        } else {
+            header.classList.remove("header-fixed");
+        }
+    }
+}())
