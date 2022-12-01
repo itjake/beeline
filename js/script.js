59a0396c1214b9dbc41c4ccd1e25156506bb500a
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-12-01 19:30:38 +0600

    refactor: changed some code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
-//header-fixed handle
-
 (function () {
-  const header = document.querySelector(".header");
-  const container = document.querySelector(".container");
+  const header = document.querySelector(".header__wrapper");
   window.onscroll = () => {
     if (window.pageYOffset > 50) {
       header.classList.add("header-fixed");
-      container.classList.add("container-fixed");
     } else {
       header.classList.remove("header-fixed");
     }
   };
-})()(
-  //Burger handler
-  (function () {
-    const burgerItem = document.querySelector(".burger");
-    const menu = document.querySelector(".header-close");
-    const menuCloseItem = document.querySelector(".header-nav__close");
-    burgerItem.addEventListener("click", () => {
-      menu.classList.add("header-close__active");
-      menuCloseItem.classList.add("header-nav__close__active");
-    });
-    menuCloseItem.addEventListener("click", () => {
-      menu.classList.remove("header-close__active");
-      menuCloseItem.classList.remove("header-nav__close__active");
-    });
-  })()
-);
+})();
+
+(function () {
+  const burgerMenu = document.querySelector(".burger-menu__btn");
+  const burgerHidden = document.querySelector(".burger-hidden-menu");
+  const burgerClose = document.querySelector(".burger-hidden__btn-close");
+  burgerMenu.addEventListener("click", () => {
+    burgerHidden.classList.add("show-burger");
+  });
+  burgerClose.addEventListener("click", () => {
+    console.log("qwerty");
+    burgerHidden.classList.remove("show-burger");
+  });
+})();
+
+(function () {
+  const beeline = document.querySelector(".beeline-dropdown");
+  const beelineList = document.querySelector(".burger-beeline__list");
+  const arrow = document.querySelector(".beeline-arrow");
+  const beelineTwo = document.querySelector(".beeline-dropdown__two");
+  const beelineListTwo = document.querySelector(".burger-beeline__list__two");
+  const arrowTwo = document.querySelector(".beeline-arrow__two");
+  beeline.addEventListener("click", () => {
+    beelineList.classList.toggle("burger-beeline__list__active");
+    arrow.classList.toggle("beeline-arrow__active");
+  });
+  beelineTwo.addEventListener("click", () => {
+    beelineListTwo.classList.toggle("burger-beeline__list__active");
+    arrowTwo.classList.toggle("beeline-arrow__active");
+  });
+})();
